a508608634a330760bedcd061e60877cf17a1be0
Billy Reisinger <billy.reisinger@gmail.com>
2021-12-01 13:33:53 -0600

    second part of day 1

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -1,17 +1,21 @@
 defmodule Day01 do
   def main(_args) do
     IO.puts inspect(count_increases())
+    IO.puts inspect(count_increases(windowed_sums()))
   end
 
-  def count_increases do
-    Enum.reduce(0..length(input())-1, fn(index, acc) ->
-      [prev, current] = Enum.slice(input(), index - 1, 2)
-      if current - prev > 0 do
-        acc + 1
-      else
-        acc
+  def count_increases(arr \\ input()) do
+    Enum.reduce(0..length(arr)-1, fn(index, acc) ->
+      [prev, current] = Enum.slice(arr, index - 1, 2)
+      cond do
+        current - prev > 0 -> acc + 1
+        true -> acc
       end
     end)
+  end
+
+  def windowed_sums do
+    Enum.chunk_every(input(), 3, 1, :discard) |> Enum.map(&Enum.sum/1)
   end
 
   def input do
